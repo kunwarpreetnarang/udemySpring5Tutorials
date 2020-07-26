d547dcaaa497993417255ed65f96d3f1bbbf6103
kunwarpreetnarang <kunwarpreet97singh@gmail.com>
2020-07-26 18:05:54 +0530

    Udemy spring5 tutorial Added external chuck jokes configuration app

---
+++ src/main/java/com/udemySpringExample1/udemySpringExample1/jokesApp/Services/impl/JokesServiceImpl.java
@@ -0,0 +1,22 @@
+package com.udemySpringExample1.udemySpringExample1.jokesApp.Services.impl;
+
+import com.udemySpringExample1.udemySpringExample1.jokesApp.Services.JokesService;
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesServiceImpl implements JokesService{
+
+    private  final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    @Autowired
+    public JokesServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJokes() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
