eb8c71b55955adfe2a9d94feb45495eed26824b2
kunwarpreetnarang <kunwarpreet97singh@gmail.com>
2020-08-02 18:56:09 +0530

    Udemy spring5 tutorial: Using Environment annotation to fetch environment values

--- src/main/java/com/udemySpringExample1/udemySpringExample1/externalProperties/config/PropertiesConfiguration.java
+++ src/main/java/com/udemySpringExample1/udemySpringExample1/externalProperties/config/PropertiesConfiguration.java
@@ -1,15 +1,24 @@
 package com.udemySpringExample1.udemySpringExample1.externalProperties.config;
 
 import com.udemySpringExample1.udemySpringExample1.externalProperties.DataLoader.FakeDataLoader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
+/*
+*
+@PropertySources(
+        @PropertySource("classpath:abc.properties"),
+        @PropertySource("classpath:xyz.properties")
+)
+*/
 public class PropertiesConfiguration {
     @Value("${mysql.username}")
     String user;
@@ -20,15 +29,17 @@ public class PropertiesConfiguration {
     @Value("${mysql.url}")
     String url;
 
+    @Autowired
+    Environment env;
+
     @Bean
-    FakeDataLoader getFakeDataLoader(){
+    FakeDataLoader getFakeDataLoader() {
         FakeDataLoader fakeDataLoader = new FakeDataLoader();
         fakeDataLoader.setUserName(user);
-        fakeDataLoader.setPassword(password);
+        fakeDataLoader.setPassword(env.getProperty("PASSWORD"));
         fakeDataLoader.setUrl(url);
         return fakeDataLoader;
     }
-    //static Environment environment;
 
     // As per spring 5 framework doc we don't need to define below bean any more.
     // If you are not using @PropertySource , then you use PropertySourcesPlaceholderConfig like this
