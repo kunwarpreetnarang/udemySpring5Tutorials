13184fd67b6f875b138c8a82f28aa883ba7f85df
kunwarpreetnarang <kunwarpreet97singh@gmail.com>
2020-08-03 15:33:42 +0530

    Udemy spring5 tutorial: Using application.yml to fetch property values

---
+++ src/main/java/com/udemySpringExample1/udemySpringExample1/externalProperties/DataLoader/FakeJmsLoader.java
@@ -0,0 +1,32 @@
+package com.udemySpringExample1.udemySpringExample1.externalProperties.DataLoader;
+
+public class FakeJmsLoader {
+
+    String username;
+    String password;
+    String url;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
--- src/main/java/com/udemySpringExample1/udemySpringExample1/externalProperties/ExternalPropertiesApplication.java
+++ src/main/java/com/udemySpringExample1/udemySpringExample1/externalProperties/ExternalPropertiesApplication.java
@@ -1,6 +1,7 @@
 package com.udemySpringExample1.udemySpringExample1.externalProperties;
 
 import com.udemySpringExample1.udemySpringExample1.externalProperties.DataLoader.FakeDataLoader;
+import com.udemySpringExample1.udemySpringExample1.externalProperties.DataLoader.FakeJmsLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,10 +12,20 @@ public class ExternalPropertiesApplication {
     public static void main(String[] args) {
 
         ApplicationContext context = SpringApplication.run(ExternalPropertiesApplication.class, args);
+
         FakeDataLoader fakeDataLoader = context.getBean(FakeDataLoader.class);
+
+        System.out.println("------- MySQL information loading -------------");
         System.out.println("UserName: " + fakeDataLoader.getUserName());
         System.out.println("Password: " + fakeDataLoader.getPassword());
         System.out.println("URL: " + fakeDataLoader.getUrl());
+
+        System.out.println("------- JMS information loading -------------");
+        FakeJmsLoader fakeJmsLoader = context.getBean(FakeJmsLoader.class);
+        System.out.println("UserName: " + fakeJmsLoader.getUsername());
+        System.out.println("Password: " + fakeJmsLoader.getPassword());
+        System.out.println("URL: " + fakeJmsLoader.getUrl());
+
     }
 
 }
