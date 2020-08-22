795cd23a736fe8da61a03dae67500db349a44983
kunwarpreetnarang <kunwarpreet97singh@gmail.com>
2020-08-23 00:22:50 +0530

    Udemy spring5 tutorial:Update existing recipe

--- src/main/java/com/udemySpringExample1/udemySpringExample1/recipieApp/Controller/IndexController.java
+++ src/main/java/com/udemySpringExample1/udemySpringExample1/recipieApp/Controller/IndexController.java
@@ -8,8 +8,10 @@ import com.udemySpringExample1.udemySpringExample1.recipieApp.Service.RecipieSer
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Optional;
 
 @Controller
