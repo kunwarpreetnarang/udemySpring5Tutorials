5c34e5248a59ef1a7686795b929bfd3a12f0a2b1
kunwarpreetnarang <kunwarpreet97singh@gmail.com>
2021-03-14 13:37:50 +0530

    Implement actuator metrics in spring app

--- src/main/java/com/udemySpringExample1/udemySpringExample1/udemystudent/UdemySpringExample1Application.java
+++ src/main/java/com/udemySpringExample1/udemySpringExample1/udemystudent/UdemySpringExample1Application.java
@@ -1,9 +1,11 @@
 package com.udemySpringExample1.udemySpringExample1.udemystudent;
 
+//import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableAdminServer
 public class UdemySpringExample1Application {
 
 	public static void main(String[] args) {
